4a02c43388b3b63896deefbfa742d99ec395aa15
my_pet_project-admin <madi.toleukul@yandex.ru>
2026-04-28 13:39:02 +0500

    exercise 3, Devicetree

--- app/src/main.cpp
+++ app/src/main.cpp
@@ -1,33 +1,35 @@
-#include <zephyr/device.h>
 #include <zephyr/drivers/gpio.h>
 #include <zephyr/kernel.h>
+#include <zephyr/logging/log.h>
 
-static const struct device *gpio_ct_dev = DEVICE_DT_GET(DT_NODELABEL(gpio0));
+#define LED_NODE DT_NODELABEL(app_led)
+
+LOG_MODULE_REGISTER(main, LOG_LEVEL_INF);
+
+static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED_NODE, gpios);
 
 int main(void)
 {
-  if (!device_is_ready(gpio_ct_dev)) {
-    return -1;
+  bool led_state = true;
+  if (!gpio_is_ready_dt(&led)) {
+    return 0;
   }
 
-  int ret;
-  ret = gpio_pin_configure(gpio_ct_dev,
-			   2, GPIO_OUTPUT_ACTIVE);
-  if (ret != 0) {
-    return -1;
-  }
   
-  while (true) {
-    ret = gpio_pin_set_raw(gpio_ct_dev, 2, 1);
-    if (ret != 0) {
-      return -1;
-    }
-    k_msleep(CONFIG_BLINK_SLEEP_TIME_MS);
-
-    ret = gpio_pin_set_raw(gpio_ct_dev, 2, 0);
-    if (ret != 0) {
-      return -1;
-    }
-    k_msleep(CONFIG_BLINK_SLEEP_TIME_MS);
+  if ( gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE) < 0) {
+    return 0;
   }
+
+  while (true) {
+   if (gpio_pin_toggle_dt(&led) < 0) {
+    return 0;
+   }
+
+   led_state = !led_state;
+
+   LOG_INF("LED state: %s", led_state ? "ON" : "OFF");
+   k_msleep(CONFIG_APP_HEARBEAT_PERIOD_MS);
+  
+  }
+  return 0;
 }
